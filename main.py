54892cc174b8979503fc2392499ed07ef5ab124e
Ericc2006 <118733715+Ericc2006@users.noreply.github.com>
2022-12-05 13:27:53 +0200

    murnieks

--- main.py
+++ main.py
@@ -9,7 +9,13 @@ def main():
 		print("""
     	1. Atņemšana
     	2. Reizināšana
-   		3. Beigt darbu ar programmu
+		3. kāpinājums pakāpe
+		4. saskaitīšana
+		5. atņemšana
+		6. skaitļu pārbaude
+		7. vidējais aritmetiskais
+   		8. Beigt darbu ar programmu
+		
             """)
 		izvele = int(input("Ievadi izvēli: "))
 		a = int(input("Ievadi 1 skaitli: "))
@@ -19,6 +25,16 @@ def main():
 		elif izvele == 2:
 			erik.reiz(a,y)
 		elif izvele == 3:
+			erik.kap(a,y)
+		elif izvele == 4:
+			linard.summa(a,y)
+		elif izvele == 5:
+			linard.atn(a,y)
+		elif izvele == 6:
+			linard.liel(a,y)
+		elif izvele == 7:
+			linard.vidap(a,y)
+		elif izvele == 8:
 			break
 
 main()
