7f103a02103e5661063e868a15312c01cdf05f4b
Ericc2006 <118733715+Ericc2006@users.noreply.github.com>
2022-12-05 12:35:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
-import erik
-import linard
+
+# uzdevuma risinājuma funkcijas katrs uzrakstījis savā failā un šos failus jāimportē pamatfailā
+import erik 
+import linard 
+# ...............
+ 
+def main():
+while True:
+        print("""
+    		1. darbība
+    	2. darbība
+	…………..
+   		3. Beigt darbu ar programmu
+            """)
+    	izvele = int(input("Ievadi izvēli: "))
+    	if izvele == 1:
+            		funkcija1()
+    	elif izvele == 2:
+      	  	funkcija2()
+    	elif izvele == 3:
+        		break 
+main()
